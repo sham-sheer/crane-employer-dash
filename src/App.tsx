d4af7331a9d2ca607b821ac2db30e0b7dd05c65e
Shamsheer Ahamed <shamsheer619@gmail.com>
2023-03-16 00:33:45 +0800

    Renamed employer dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -291,7 +291,7 @@ export default function App() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Crane
+                  Crane for Employers
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600">
                   We specialize in connecting companies with the skilled and
@@ -299,16 +299,10 @@ export default function App() {
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
-                    href="#"
+                    onClick={() => navigate("/login")}
                     className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
                   >
                     Get started
-                  </a>
-                  <a
-                    href="#"
-                    className="text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
                   </a>
                 </div>
               </div>
